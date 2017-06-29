d27aa03c81e09defecd3e4eaa3c8808b7bb02f3e
Joe Kim <josephkim0224@gmail.com>
2017-06-29 14:16:53 -0400

    Fix navigation links, add dummy form, and add extra route prefixes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   get '/signin' => "sessions#signin"
-  post '/signin' => 'sessions#signin_post'
+  post '/signin' => 'sessions#signin_post', as: "signin_post"
   get '/signup' => 'sessions#signup'
-  post '/signup' => 'sessions#signup_post'
+  post '/signup' => 'sessions#signup_post', as: "signup_post"
   delete "/signout" => 'sessions#destroy'
 end
