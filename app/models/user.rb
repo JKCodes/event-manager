2ed4fb0b291906c35d772ba9ebd5823791a078fa
Joe Kim <josephkim0224@gmail.com>
2017-06-29 08:49:24 -0400

    User validation complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :participants
   has_many :events
   has_many :locations
+
+  has_secure_password
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true
+  validates :password, length: { minimum: 8 }
 end
