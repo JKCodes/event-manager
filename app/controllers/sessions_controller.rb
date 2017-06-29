1dfb3a61643c346b4678220effd871c5718152fe
Joe Kim <josephkim0224@gmail.com>
2017-06-29 13:49:57 -0400

    Fill out sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.user
+      redirect_to root_path, notice: "Welcome back to Event Manager!"
+    else
+      redirect_to signin_path
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to root_path
+  end
+end
