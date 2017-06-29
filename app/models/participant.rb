95b36e6efdbe9482261b3512297149268e2ca189
Joe Kim <josephkim0224@gmail.com>
2017-06-29 08:56:11 -0400

    Participant validation complete

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,4 +3,6 @@ class Participant < ApplicationRecord
   has_many :events, through: :event_participants
   has_many :event_participants
   has_many :locations, through: :events
+
+  validates :nickname, presence: true
 end
