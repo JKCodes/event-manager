f971a31165bc82c33781b16b1304406559529dae
Joe Kim <josephkim0224@gmail.com>
2017-06-29 13:39:48 -0400

    main layout complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :signed_in?
+  helper_method :current_user
+
+  def signed_in?
+    !!current_user
+  end
+
+  def current_user
+    # returns a dummy user until session controller is defined
+    User.new
+  end
 end
