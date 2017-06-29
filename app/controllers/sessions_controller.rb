8ffc707aff2503522fd19b7795c5b2dd804c2011
Joe Kim <josephkim0224@gmail.com>
2017-06-29 15:28:58 -0400

    sign in complete and fixed form error partial issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,16 @@ class SessionsController < ApplicationController
   def signin_post
     user = User.find_by(username: params[:user][:username])
     if user && user.authenticate(params[:user][:password])
-      session[:user_id] = user.user
+      session[:user_id] = user.id
       redirect_to root_path, notice: "Welcome back to Event Manager!"
     else
-      redirect_to signin_path
+      @user = User.new(session_params)
+      @user.valid?
+      @user.errors.delete :email
+      @user.errors.delete :username if user
+      @user.errors.add(:base, "Username/password combination not found") if params[:user][:password].length >= 8 && @user.errors.empty?
+  
+      render :signin
     end
   end
 
@@ -18,11 +24,12 @@ class SessionsController < ApplicationController
   end
 
   def signup_post
-    @user = User.new(session_params)
-    if @user.save
-      session[:user_id] = @user.id
+    user = User.new(session_params)
+    if user.save
+      session[:user_id] = user.id
       redirect_to root_path, notice: "Welcome to Event Manager!"
     else
+      @user = user
       render :signup
     end
   end
