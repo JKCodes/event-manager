48f87a667f9fe5f2b4a485236ae93c8fbe33782f
Joe Kim <josephkim0224@gmail.com>
2017-06-29 08:37:21 -0400

    fill out locations, participants, and event_participant migrations

--- db/migrate/20170629122725_create_locations.rb
+++ db/migrate/20170629122725_create_locations.rb
@@ -1,6 +1,12 @@
 class CreateLocations < ActiveRecord::Migration[5.1]
   def change
     create_table :locations do |t|
+      t.string :name
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zipcode
+      t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20170629122735_create_participants.rb
+++ db/migrate/20170629122735_create_participants.rb
@@ -1,6 +1,12 @@
 class CreateParticipants < ActiveRecord::Migration[5.1]
   def change
     create_table :participants do |t|
+      t.string :nickname
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      t.integer :user_id
 
       t.timestamps
     end
