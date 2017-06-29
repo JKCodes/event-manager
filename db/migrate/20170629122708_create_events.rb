0a5b275b6549255c124ba881db756bfef13869c5
Joe Kim <josephkim0224@gmail.com>
2017-06-29 08:32:35 -0400

    fill out user and event migrations

--- db/migrate/20170629122708_create_events.rb
+++ db/migrate/20170629122708_create_events.rb
@@ -1,6 +1,11 @@
 class CreateEvents < ActiveRecord::Migration[5.1]
   def change
     create_table :events do |t|
+      t.string :name
+      t.datetime :start_datetime
+      t.integer :duration
+      t.integer :user_id
+      t.integer :location_id
 
       t.timestamps
     end
