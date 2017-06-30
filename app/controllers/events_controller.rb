e3c05065190b312f48da83b82ffae3a2cfd64027
Joe Kim <josephkim0224@gmail.com>
2017-06-30 11:02:41 -0400

    Event show page finished

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,14 @@
 class EventsController < ApplicationController
   before_action :redirect_if_not_signed_in
+  before_action :set_event, only: [:show]
 
   def index
     @todays_events = Event.by_user(current_user).starts_today
     @upcoming_events = Event.by_user(current_user).upcoming_events
   end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
 end
--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -1,2 +1,7 @@
 module ParticipantsHelper
+  def participant_to_s(participant)
+    full_name = "#{participant.first_name} #{participant.last_name}"
+    full_name == " " ? "#{participant.nickname}" : full_name + "(#{participant.nickname})"
+  end
+
 end
