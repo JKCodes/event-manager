cba3348223075a583ca0de62b2b78a811f9563c3
Joe Kim <josephkim0224@gmail.com>
2017-06-30 15:22:44 -0400

    event delete complete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :redirect_if_not_signed_in
-  before_action :set_event, only: [:show, :edit, :update]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :create_event, only: [:new]
 
   def index
@@ -29,6 +29,11 @@ class EventsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path, notice: "Event was deleted successfully."
   end
 
   private
