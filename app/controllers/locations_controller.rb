fde6fb9d2fd23c969650bb8a9e10df756b3896e7
Joe Kim <josephkim0224@gmail.com>
2017-06-30 15:41:48 -0400

    new location form complete

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,13 @@
 class LocationsController < ApplicationController
   before_action :redirect_if_not_signed_in
+  before_action :create_location
 
   def index
     @locations = Location.by_user(current_user)
   end
+
+  private
+    def create_location
+      @location = current_user.locations.build
+    end
 end
