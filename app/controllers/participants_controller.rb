8183183d161354e6b450aa331eeba4f222e73020
Joe Kim <josephkim0224@gmail.com>
2017-06-30 16:15:14 -0400

    finish standard crud methods on controller

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -3,6 +3,33 @@ class ParticipantsController < ApplicationController
   before_action :create_participant, only: [:new]
   before_action :set_participant, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @participants = Participant.by_user(current_user)
+  end
+
+  def create
+    @participant = current_user.participants.build(participant_params)
+
+    if @participant.save
+      redirect_to participants_path, notice: "Participant was created successfully"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @participant.update(participant_params)
+      redirect_to participant_path(@participant), notice: "Participant was updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @participant.destroy
+    redirect_to participants_path, notice: "Participant was deleted successfully"
+  end
+
   private
     def create_participant
       @participant = current_user.participants.build
