5d6ba6633363dd03988bcc9b1ed593d66f17e7ea
Joe Kim <josephkim0224@gmail.com>
2017-06-30 15:46:15 -0400

    create location complete

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,8 +6,22 @@ class LocationsController < ApplicationController
     @locations = Location.by_user(current_user)
   end
 
+  def create
+    @location = current_user.locations.build(location_params)
+
+    if @location.save
+      redirect_to locations_path, notice: "Location was created successfully"
+    else
+      render :new
+    end
+  end
+
   private
     def create_location
       @location = current_user.locations.build
+    end
+
+    def location_params
+      params.require(:location).permit(:name, :street_address, :city, :state, :zipcode)
     end
 end
