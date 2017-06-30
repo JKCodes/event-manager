97ab09d9a78c03fa9d02fd6ad2faa1116f2ad59d
Joe Kim <josephkim0224@gmail.com>
2017-06-30 16:13:17 -0400

    Participant before_action methods

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,3 +1,18 @@
 class ParticipantsController < ApplicationController
   before_action :redirect_if_not_signed_in
+  before_action :create_participant, only: [:new]
+  before_action :set_participant, only: [:show, :edit, :update, :destroy]
+
+  private
+    def create_participant
+      @participant = current_user.participants.build
+    end
+
+    def set_participant
+      @participant = Participant.by_user(current_user).find(params[:id])
+    end
+
+    def participant_params
+      params.require(:participant).permit(:name, :street_address, :city, :state, :zipcode)
+    end
 end
