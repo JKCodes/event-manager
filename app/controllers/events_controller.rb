9634575b760943a37674188fd97064efa3a45190
Joe Kim <josephkim0224@gmail.com>
2017-06-30 10:33:58 -0400

    Finish events index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,8 @@
 class EventsController < ApplicationController
   before_action :redirect_if_not_signed_in
+
+  def index
+    @todays_events = Event.by_user(current_user).starts_today
+    @upcoming_events = Event.by_user(current_user).upcoming_events
+  end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+  def formatted_time(time)
+    time.strftime("%B %e, %Y - %l:%M %p")
+  end
 end
