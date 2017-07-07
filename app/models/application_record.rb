2ba269736d138faa4fac33eee516f803f9ad92f6
Joe Kim <josephkim0224@gmail.com>
2017-07-07 16:54:10 -0400

    refactored previous/next methods to applicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,4 +4,16 @@ class ApplicationRecord < ActiveRecord::Base
   def self.by_user(user_id)
     where(user: user_id)
   end
+
+  def self.previous(object)
+    objects = self.by_user(object.user)
+    new_index = objects.index(object) - 1
+    new_index < 0 ? objects.last : objects[new_index]
+  end
+
+  def self.next(object)
+    objects = self.by_user(object.user)
+    new_index = objects.index(object) + 1
+    new_index == objects.size ? objects.first : objects[new_index]
+  end
 end
