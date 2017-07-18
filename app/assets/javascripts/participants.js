b60597a4d068fa431b6f14f9080d8baf71f0212b
Joe Kim <josephkim0224@gmail.com>
2017-07-18 10:21:43 -0400

    Refactored load events out of Participant.js into Paricipant's own function

--- app/assets/javascripts/participants.js
+++ app/assets/javascripts/participants.js
@@ -38,27 +38,29 @@ Participant.clickNavigate = function(e) {
   });
 }
 
+Participant.clickLoadParticipantEvents = function(e) {
+  e.preventDefault();
+
+  $.get(this.href).success(function(json) {
+    $("a.load_participant_events").hide()
+
+    var $events = $("div.events")
+    var html = "<ol>"
+
+    json.forEach(function(event) {
+      html += `<li><a href="/events/${event.id}">${event.name}</a></li>`
+    })
+
+    html += "</ol>"
+
+    $events.html(html)
+  });
+}
+
 $(document).on('turbolinks:load', function() {
 
   $("a.load_next_participant, a.load_previous_participant").on("click", Participant.clickNavigate);
 
   // Participant show events
-  $("a.load_participant_events").on("click", function(e) {
-    e.preventDefault();
-
-    $.get(this.href).success(function(json) {
-      $("a.load_participant_events").hide()
-
-      var $events = $("div.events")
-      var html = "<ol>"
-
-      json.forEach(function(event) {
-        html += `<li><a href="/events/${event.id}">${event.name}</a></li>`
-      })
-
-      html += "</ol>"
-
-      $events.html(html)
-    });
-  });  
+  $("a.load_participant_events").on("click", Participant.clickLoadParticipantEvents);  
 });
