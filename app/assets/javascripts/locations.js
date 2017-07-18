e14934d694fa751e5317eb02e763cb5c6bbf2d89
Joe Kim <josephkim0224@gmail.com>
2017-07-18 09:43:28 -0400

    Refactor location show events ajax logic into a new Location function

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -65,28 +65,28 @@ Location.clickNavigate = function(e) {
   });
 }
 
+Location.clickLoadLocationEvents = function(e) {
+  e.preventDefault();
+
+  $.get(this.href).success(function(json) {
+    $("a.load_location_events").hide()
+
+    var $events = $("div.events")
+    var html = "<ol>"
+
+    json.forEach(function(event) {
+      html += `<li><a href="/events/${event.id}">${event.name}</a></li>`
+    })
+
+    html += "</ol>"
+
+    $events.html(html)
+  });
+}
+
 $(document).on('turbolinks:load', function() {
 
   $("#new_location").on('submit', Location.formSubmit);
   $("a.load_next_location, a.load_previous_location").on("click", Location.clickNavigate);  
-
-  // Location show events
-  $("a.load_location_events").on("click", function(e) {
-    e.preventDefault();
-
-    $.get(this.href).success(function(json) {
-      $("a.load_location_events").hide()
-
-      var $events = $("div.events")
-      var html = "<ol>"
-
-      json.forEach(function(event) {
-        html += `<li><a href="/events/${event.id}">${event.name}</a></li>`
-      })
-
-      html += "</ol>"
-
-      $events.html(html)
-    });
-  });  
+  $("a.load_location_events").on("click", Location.clickLoadLocationEvents);  
 });
