0da5597142b3649aedd65a9a8e64f52b74f5af34
Joe Kim <josephkim0224@gmail.com>
2017-07-18 09:40:45 -0400

    Refactor Location show page navigation into a new Location function

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -37,37 +37,38 @@ Location.formSubmit = function(e) {
   });
 }
 
+Location.clickNavigate = function(e) {
+  e.preventDefault();
+
+  const indexes = $(this).data("indexes")
+  const current_id = +$(this).attr("data-current")
+  const current_index = indexes.indexOf(current_id)
+  var modified_index = $(this).attr('class') === "load_next_location" ? current_index + 1 : current_index - 1
+  if (modified_index >= indexes.length) { modified_index = 0; }
+  if (modified_index < 0) { modified_index = indexes.length - 1; }
+  const previous_index = modified_index > 0 ? modified_index - 1 : indexes.length - 1
+  const next_index = modified_index < indexes.length - 1 ? modified_index + 1 : 0
+  const previous_id = indexes[previous_index]
+  const next_id = indexes[next_index]
+
+  $.get(this.href).success(function(json) {
+    $("div.location_events").hide()
+    $("a.load_previous_location")[0].href = "/locations/" + previous_id
+    $("a.load_next_location")[0].href = "/locations/" + next_id
+    $("a.load_previous_location").attr("data-current", json.id)
+    $("a.load_next_location").attr("data-current", json.id)
+    $("#name").html("Name: " + json.name)
+    $("#street_address").html("Street Address: " + json.street_address)
+    $("#city").html("City: " + json.city)
+    $("#state").html("State: " + json.state)
+    $("#zipcode").html("Zipcode: " + json.zipcode)
+  });
+}
+
 $(document).on('turbolinks:load', function() {
 
   $("#new_location").on('submit', Location.formSubmit);
-
-  $("a.load_next_location, a.load_previous_location").on("click", function(e) {
-    e.preventDefault();
-
-    const indexes = $(this).data("indexes")
-    const current_id = +$(this).attr("data-current")
-    const current_index = indexes.indexOf(current_id)
-    var modified_index = $(this).attr('class') === "load_next_location" ? current_index + 1 : current_index - 1
-    if (modified_index >= indexes.length) { modified_index = 0; }
-    if (modified_index < 0) { modified_index = indexes.length - 1; }
-    const previous_index = modified_index > 0 ? modified_index - 1 : indexes.length - 1
-    const next_index = modified_index < indexes.length - 1 ? modified_index + 1 : 0
-    const previous_id = indexes[previous_index]
-    const next_id = indexes[next_index]
-
-    $.get(this.href).success(function(json) {
-      $("div.location_events").hide()
-      $("a.load_previous_location")[0].href = "/locations/" + previous_id
-      $("a.load_next_location")[0].href = "/locations/" + next_id
-      $("a.load_previous_location").attr("data-current", json.id)
-      $("a.load_next_location").attr("data-current", json.id)
-      $("#name").html("Name: " + json.name)
-      $("#street_address").html("Street Address: " + json.street_address)
-      $("#city").html("City: " + json.city)
-      $("#state").html("State: " + json.state)
-      $("#zipcode").html("Zipcode: " + json.zipcode)
-    });
-  });  
+  $("a.load_next_location, a.load_previous_location").on("click", Location.clickNavigate);  
 
   // Location show events
   $("a.load_location_events").on("click", function(e) {
